f80d0608bf431767bb08ca343923220d1980ac84
domihtml <dvorak.dominika@gmail.com>
2021-04-04 13:38:38 +0200

    Hunter moving (onkeydown)

--- main.js
+++ main.js
@@ -8,10 +8,14 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 // sem začni psát svůj program
 
+
+//globální proměnné:
 let hunter, hunterX, hunterY, hunterWidth, hunterHeight;
 let coin, coinX, coinY, coinWidth, coinHeight;
 
-function pageOnLoad (){
+
+//funkce při načtení stránky
+function startingPosition (){
 	hunter = document.getElementById("panacek");
 	coin = document.getElementById("mince");
 	hunterWidth = hunter.width;
@@ -19,6 +23,8 @@ function pageOnLoad (){
 
 	hunterX = Math.round(window.innerWidth / 2 - hunterWidth / 2);
 	hunterY = Math.round(window.innerHeight / 2 - hunterHeight / 2);
+
+	console.log (hunterX)
 
 	placeHunter()
 
@@ -28,14 +34,58 @@ function pageOnLoad (){
 	newPositionCoin()
 }
 
+//umístění huntera na stránce na střed
 function placeHunter () {
 	hunter.style.left = hunterX + "px";
 	hunter.style.top = hunterY + "px";
 }
 
+//náhodné umístění mince na stránce
 function newPositionCoin() {
 	coinX = Math.round(Math.random() * (window.innerWidth - coinWidth));
 	coinY = Math.round(Math.random()* (window.innerHeight - coinHeight));
 	coin.style.left = coinX + "px";
 	coin.style.top = coinY + "px";
+}
+
+
+function hunterMoving(udalost) {
+	if (udalost.key === "ArrowLeft") {
+		hunterX -= 10;
+		if (hunterX < 0) {
+			hunterX = 0;
+		}
+		hunter.src = "obrazky/panacek-vlevo.png";
+		console.log (hunterX);
+	}
+
+	if (udalost.key === "ArrowRight") {
+		hunterX += 10;
+		if (hunterX + hunterWidth > window.innerWidth) {
+			hunterX = window.innerWidth - hunterWidth;
+		}
+		hunter.src = "obrazky/panacek-vpravo.png";
+		console.log (hunterX);
+	}
+
+	
+	if (udalost.key === "ArrowUp") {
+		hunterY -= 10;
+		if (hunterY < 0) {
+			hunterY = 0;
+		}
+		hunter.src = "obrazky/panacek-nahoru.png";
+		console.log (hunterY);
+	}
+
+
+	if (udalost.key === "ArrowDown") {
+		hunterY += 10;
+		if (hunterY + hunterHeight > window.innerHeight) {
+			hunterY = window.innerHeight - hunterHeight;
+		}
+		hunter.src = "obrazky/panacek.png";
+		console.log (hunterX);
+	}
+	placeHunter()
 }
