6cc469fdc1d910159194e769395f576c1803dbab
domihtml <dvorak.dominika@gmail.com>
2021-04-04 12:23:58 +0200

    Výchozí pozice huntera a mince při načtení stránky

--- main.js
+++ main.js
@@ -7,3 +7,35 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
+let hunter, hunterX, hunterY, hunterWidth, hunterHeight;
+let coin, coinX, coinY, coinWidth, coinHeight;
+
+function pageOnLoad (){
+	hunter = document.getElementById("panacek");
+	coin = document.getElementById("mince");
+	hunterWidth = hunter.width;
+	hunterHeight = hunter.height;
+
+	hunterX = Math.round(window.innerWidth / 2 - hunterWidth / 2);
+	hunterY = Math.round(window.innerHeight / 2 - hunterHeight / 2);
+
+	placeHunter()
+
+	coinWidth = coin.width;
+	coinHeight = coin.height;
+
+	newPositionCoin()
+}
+
+function placeHunter () {
+	hunter.style.left = hunterX + "px";
+	hunter.style.top = hunterY + "px";
+}
+
+function newPositionCoin() {
+	coinX = Math.round(Math.random() * (window.innerWidth - coinWidth));
+	coinY = Math.round(Math.random()* (window.innerHeight - coinHeight));
+	coin.style.left = coinX + "px";
+	coin.style.top = coinY + "px";
+}
